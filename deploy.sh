abb4862558da81190c6d9675b905088076867932
ReverseBrain <reversebrain@protonmail.com>
2020-11-29 23:31:48 +0100

    Publishing to main

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git clone https://github.com/PGiatasti/pgiatasti.github.io -b gh-pages public
 
 echo "Removing existing files"
-rm -rf public/*
+cd public
+rm -rf $(ls | grep -v CNAME | grep -v '.git')
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages"
+git add --all && git commit -m "Publishing to gh-pages" || exit 1
 
 echo "Pushing to github"
 git push origin gh-pages
+
+cd ..
+rm -rf public
+git add --all && git commit -m "Publishing to main"
+git push origin main
